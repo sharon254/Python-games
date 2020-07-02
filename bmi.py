55931d649a4b4267684209a1e9dbd679f3d22657
Sharon Nyabuti <sharonknyabuti@gmail.com>
2020-07-02 14:12:47 +0300

    Added bmi.py

---
+++ bmi.py
@@ -0,0 +1,21 @@
+height = float(input("What is your height in meters? "))
+weight = float(input("What is your weight in kilograms? "))
+
+bmi = weight / height ** 2
+print(bmi)
+if bmi < 15:
+    print("Very severely underweight")
+elif bmi < 16:
+    print("Severely underweight")
+elif bmi < 18.5:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal (healthy weight)")
+elif bmi < 30:
+    print("Overweight")
+elif bmi < 35:
+    print("Obese Class I (Moderately obese)")
+elif bmi < 40:
+    print("Obese Class II (Severely obese)")
+else:
+    print("Obese Class III (Very severely obese)")
